a3222c274f9377c0600cdccf47bdfeefbff3f7a0
NuttCorp <34084918+NuttCorp@users.noreply.github.com>
2020-05-08 20:13:55 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-
-sed -i 's/db_user/'$MYSQL_USER'/g' /var/www/uvdesk/.env
-sed -i 's/db_password/'$MYSQL_PASSWORD'/g' /var/www/uvdesk/.env
-sed -i 's/127.0.0.1/'$MYSQL_HOST'/g' /var/www/uvdesk/.env
-sed -i 's/db_name/'$MYSQL_DATABASE'/g' /var/www/uvdesk/.env
   
 service apache2 start
 
